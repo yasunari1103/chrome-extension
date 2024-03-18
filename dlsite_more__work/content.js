a326031f266c71779ff04dc4b54cfbdf2851c7ab
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-03-19 04:41:58 +0900

    url取得まで

---
+++ dlsite_more__work/content.js
@@ -0,0 +1,5 @@
+let urls_list = document.querySelectorAll(".page_no ul a");
+let url1 = urls_list.item(0).href;
+let url2 = urls_list.item(1).href;
+let url3 = urls_list.item(2).href;
+let url4 = urls_list.item(3).href;
