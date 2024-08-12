d4f4942635f5f4d74e1dd6933518d4a6ebc847f3
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2024-08-12 17:27:06 +0900

    ボタンの作成と配置まで終わった
    functionの修正を行う

--- DMMgames_ill_save/content.js
+++ DMMgames_ill_save/content.js
@@ -1,24 +1,6 @@
-let canvas = document.getElementById("unity-canvas");
-document.getElementById("saveCanvas").addEventListener("click", () => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: saveCanvasAsImage,
-    });
-  });
-});
+let page = document.getElementsByClassName("page-inner")[0]; //HTMLCollection object ＜＝配列なのでhtml文じゃない
+let saveCanvasButton = document.createElement("button");
 
-function saveCanvasAsImage() {
-  const canvas = document.querySelector("canvas");
-  if (canvas) {
-    const dataURL = canvas.toDataURL("image/png");
-    const link = document.createElement("a");
-    link.href = dataURL;
-    link.download = "canvas-image.png";
-    link.click();
-  } else {
-    alert("No canvas element found on this page.");
-  }
-}
+saveCanvasButton.textContent = "画像を保存！！！";
+page.appendChild(saveCanvasButton);
 
-console.log("a");
