c0cf079ca3500dae2f672fca9f339d9e7b9ce7e4
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-09-16 20:19:26 +0900

    完成～～～～

--- YT_ad_mute/content.js
+++ YT_ad_mute/content.js
@@ -1,5 +1,38 @@
-window.addEventListener("load",main,false);
+window.addEventListener("load", main, false);
 
 function main() {
-    const ads = document.getElementById('ad-avatar-lockup-card:4');
-};
+  const target = document.body; // 対象とするノードを取得
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const ads = document.getElementsByClassName("ytp-preview-ad");
+      const muteButton = document.getElementsByClassName(
+        "ytp-mute-button ytp-button"
+      )[0];
+      if (muteButton) {
+        //広告ある
+        if (ads.length > 0) {
+          if (muteButton.title !== "ミュート解除（m）") {
+            muteButton.click();
+          }
+        }
+        //広告ない
+        else if (ads.length === 0) {
+          if (muteButton.title === "ミュート解除（m）") {
+            muteButton.click();
+          }
+        }
+      }
+    });
+  });
+
+  //監視対象の設定
+  const config = {
+    childList: true,
+    attributes: true,
+    subtree: true,
+    characterData: true,
+  };
+
+  observer.observe(target, config);
+}
