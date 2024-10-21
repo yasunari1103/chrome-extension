10f88607ae6a1d7c30e480b3fb3f678d4871d954
yaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasu <129604598+yasunari1103@users.noreply.github.com>
2024-10-21 17:43:06 +0900

    ボタン作成、コンソール上の要素表示まで

--- booth_cart_integration/content.js
+++ booth_cart_integration/content.js
@@ -1,6 +1,15 @@
 (function () {
-var carts = document.getElementsByClassName('');
-var carts = [...carts] ///配列に変換
-carts.forEach((cart) => {
-    console.log(cart);
-});}) ()
+  let button = document.createElement("button");
+  button.textContent = "統合するカートを選択！";
+  button.addEventListener("click", function () {
+    var cartsCollection = document.getElementsByClassName(
+      "sheet sheet--p500 cart-box"
+    );
+    var carts = [...cartsCollection]; ///配列に変換
+    carts.forEach((cart) => {
+      console.log(cart);
+    });
+  });
+  let title = document.getElementsByClassName("manage-page-head")[0];
+  title.appendChild(button);
+})();
