5146a2a1d637fcd2eef2b5fb1eed441282ab88bb
tajima-yasunari <135994628+tajima-yasunari@users.noreply.github.com>
2024-10-21 23:43:40 +0900

    boothのほう微修正
    DMMのほうよくわからん

--- DMMgames_ill_save/content.js
+++ DMMgames_ill_save/content.js
@@ -1,5 +1,18 @@
 let page = document.getElementsByClassName("page-inner")[0]; //HTMLCollection object ＜＝配列なのでhtml文じゃない
 let saveCanvasButton = document.createElement("button");
+saveCanvasButton.addEventListener("click",function() {
+  const canvas = document.querySelector("canvas");
+  if (canvas) {
+    const dataURL = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = dataURL;
+    link.download = "canvas-image.png";
+    link.click();
+  } else {
+    alert("No canvas element found on this page.");
+  }
+}
+)
 
 saveCanvasButton.textContent = "画像を保存！！！";
 page.appendChild(saveCanvasButton);
--- booth_cart_integration/content.js
+++ booth_cart_integration/content.js
@@ -2,12 +2,12 @@
   let button = document.createElement("button");
   button.textContent = "統合するカートを選択！";
   button.addEventListener("click", function () {
-    var cartsCollection = document.getElementsByClassName(
-      "sheet sheet--p500 cart-box"
+    var cartsCollection = document.querySelectorAll(
+      ".cart-item__name .nav"
     );
     var carts = [...cartsCollection]; ///配列に変換
     carts.forEach((cart) => {
-      console.log(cart);
+      console.log(cart.href);
     });
   });
   let title = document.getElementsByClassName("manage-page-head")[0];
